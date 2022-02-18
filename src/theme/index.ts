6dc997a6fd843876498e3d4c93821454f8d9b290
luisescx <luis.esculapio@live.com>
2022-02-17 23:15:58 -0300

    Feat: added themes

---
+++ src/theme/index.ts
@@ -0,0 +1,43 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#F5FBFB',
+    SHAPE: '#EBF3F5',
+
+    TEXT_DARK: '#787994',
+    TEXT_LIGHT: '#FAFBFB',
+    TITLE: '#2E3057',
+
+    SUCCESS: '#12A454',
+    SUCCESS_LIGHT: `${'rgba(18, 164, 84, 0.3)'}`,
+
+    DANGER: '#E83F5B',
+    DANGER_LIGHT: `${'rgba(232, 63, 91, 0.3)'}`,
+
+    POKEMON_TYPES: {
+      ROCK: '#B6A136',
+      STEEL: '#B7b7CE',
+      WATER: '#6390F0',
+      BUG: '#A6B91A',
+      DARK: '#705746',
+      DRAGON: '#6f36FC',
+      ELECTRIC: '#F7d02C',
+      FAIRY: '#D685AD',
+      FIGHTING: '#c22E28',
+      FIRE: '#EE8130',
+      FLYING: '#A98FF3',
+      GHOST: '#735797',
+      GRASS: '#7AC74c',
+      GROUND: '#E2Bf65',
+      ICE: '#96D9D6',
+      NORMAL: '#A8A77A',
+      POISON: '#A33EA1',
+      PSYCHIC: '#F95587',
+    },
+  },
+
+  FONTS: {
+    REGULAR: 'Poppins-Regular',
+    MEDIUM: 'Poppins-Medium',
+    BOLD: 'Poppins-Bold',
+  },
+};
