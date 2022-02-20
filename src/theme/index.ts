33f9ea411ec68411b6963a5d59d4864fcbb51960
luisescx <luis.esculapio@live.com>
2022-02-20 20:12:22 -0300

    Feat: added skeleton colors on theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -6,12 +6,16 @@ export default {
     TEXT_DARK: '#787994',
     TEXT_LIGHT: '#FAFBFB',
     TITLE: '#2E3057',
+    TITLE_LIGHT: `${'rgba(46, 48, 87, 0.5)'}`,
 
     SUCCESS: '#12A454',
     SUCCESS_LIGHT: `${'rgba(18, 164, 84, 0.3)'}`,
 
     DANGER: '#E83F5B',
     DANGER_LIGHT: `${'rgba(232, 63, 91, 0.3)'}`,
+
+    SKELETON: '#C4D1D5',
+    SKELETON_FOREGROUND: '#E3EEF1',
 
     POKEMON_TYPES: {
       ROCK: '#B6A136',
