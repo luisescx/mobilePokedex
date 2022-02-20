4f438f51435bfacc656f78cb78ec1493a21ea442
luisescx <luis.esculapio@live.com>
2022-02-20 20:14:19 -0300

    Feat: added api interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,20 @@
-import axios from 'axios';
+import axios, {AxiosError, AxiosResponse} from 'axios';
 
 export const apiUrl = 'https://pokeapi.co/api/v2/';
 
 const api = axios.create({
   baseURL: apiUrl,
 });
+
+const eject = api.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return Promise.resolve(response);
+  },
+  async (error: AxiosError) => {
+    return error?.response;
+  },
+);
+
+axios.interceptors.request.eject(eject);
 
 export default api;
