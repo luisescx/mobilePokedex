6c55d7e57f60190e45789988ccb4faee429f6582
luisescx <luis.esculapio@live.com>
2022-02-22 21:31:51 -0300

    Fix: skeleton key

--- src/components/Skeleton/Loader/index.tsx
+++ src/components/Skeleton/Loader/index.tsx
@@ -3,33 +3,40 @@ import {Dimensions} from 'react-native';
 import {Rect} from 'react-content-loader/native';
 import {ContentLoaderStyled} from '../index';
 
-const width = Dimensions.get('screen').width;
-const height = Dimensions.get('screen').height;
-
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+const HEIGHT = Dimensions.get('screen').height;
+const CARD_WIDTH = (SCREEN_WIDTH - (48 + 16)) / 2;
+const SECOND_CARD_STARTS = 24 + CARD_WIDTH + 16;
 const RACE_HEIGHT = 250;
 const MARGIN = 16;
-
-const SKELETON_HEIGHT = height * (65 / 100);
+const SKELETON_HEIGHT = HEIGHT * (65 / 100);
 
 const PokemonCard: React.FC = () => {
   return (
-    <ContentLoaderStyled height={SKELETON_HEIGHT} width={width}>
-      <Rect x={24} y={0} height={250} width={'42.50%'} rx={20} ry={20} />
-      <Rect x={215} y={0} height={250} width={'42.50%'} rx={20} ry={20} />
+    <ContentLoaderStyled height={SKELETON_HEIGHT} width={SCREEN_WIDTH}>
+      <Rect x={24} y={0} height={250} width={CARD_WIDTH} rx={20} ry={20} />
+      <Rect
+        x={SECOND_CARD_STARTS}
+        y={0}
+        height={250}
+        width={CARD_WIDTH}
+        rx={20}
+        ry={20}
+      />
 
       <Rect
         x={24}
         y={RACE_HEIGHT + MARGIN}
         height={250}
-        width={'42.50%'}
+        width={CARD_WIDTH}
         rx={20}
         ry={20}
       />
       <Rect
-        x={215}
+        x={SECOND_CARD_STARTS}
         y={RACE_HEIGHT + MARGIN}
         height={250}
-        width={'42.50%'}
+        width={CARD_WIDTH}
         rx={20}
         ry={20}
       />
@@ -38,15 +45,15 @@ const PokemonCard: React.FC = () => {
         x={24}
         y={RACE_HEIGHT * 2 + MARGIN * 2}
         height={250}
-        width={'42.50%'}
+        width={CARD_WIDTH}
         rx={20}
         ry={20}
       />
       <Rect
-        x={215}
+        x={SECOND_CARD_STARTS}
         y={RACE_HEIGHT * 2 + MARGIN * 2}
         height={250}
-        width={'42.50%'}
+        width={CARD_WIDTH}
         rx={20}
         ry={20}
       />
