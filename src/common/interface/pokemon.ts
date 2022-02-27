5b8cba72dc2b2b26c35f595882dd0bf4c37fec6f
luisescx <luis.esculapio@live.com>
2022-02-27 10:51:11 -0300

    Feat: added new attributes on pokemon interface

--- src/common/interface/pokemon.ts
+++ src/common/interface/pokemon.ts
@@ -4,4 +4,27 @@ export interface Pokemon {
   image: string;
   types: string[];
   pokemonNumber: string;
+  about: string | null;
+  height: string;
+  weight: string;
+  stats: Stats[];
+  abilities: string[];
+  evolutionChain: EvolvesTo[] | null;
+  species: Species;
+}
+
+export interface Stats {
+  baseStat: number;
+  name: string;
+}
+
+export interface EvolvesTo {
+  id: number;
+  name: string;
+  image: string;
+}
+
+export interface Species {
+  name: string;
+  url: string;
 }
