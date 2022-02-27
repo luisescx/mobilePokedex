786f4f4bd24a79ebd09b22ada248ec573d4bf74b
luisescx <luis.esculapio@live.com>
2022-02-27 10:54:52 -0300

    Feat: added new pokemonDTOs interfaces

--- src/dtos/pokemon.ts
+++ src/dtos/pokemon.ts
@@ -3,6 +3,38 @@ export interface PokemonDTO {
   name: string;
   sprites: SpritesDTO;
   types: TypeDTO[];
+  height: number;
+  weight: number;
+  stats: StatsDTO[];
+  abilities: Ability[];
+  species: speciesDTO;
+}
+
+export interface StatsDTO {
+  base_stat: number;
+  stat: {
+    name: string;
+  };
+}
+
+export interface Ability {
+  ability: {
+    name: string;
+  };
+}
+
+export interface PokemonSpeciesDTO {
+  flavor_text_entries: [
+    {
+      flavor_text: string;
+      language: {
+        name: string;
+      };
+    },
+  ];
+  evolution_chain: {
+    url: string;
+  };
 }
 
 interface SpritesDTO {
@@ -19,4 +51,22 @@ interface TypeDTO {
     name: string;
     url: string;
   };
+}
+
+export interface EvolutionChainDTO {
+  id: number;
+  chain: {
+    species: speciesDTO;
+    evolves_to: EvolvesToDTO[];
+  };
+}
+
+export interface EvolvesToDTO {
+  species: speciesDTO;
+  evolves_to: EvolvesToDTO[];
+}
+
+interface speciesDTO {
+  name: string;
+  url: string;
 }
