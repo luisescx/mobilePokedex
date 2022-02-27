2a60f8184b14825d228dd0f845de2d5e128fdd02
luisescx <luis.esculapio@live.com>
2022-02-27 20:55:15 -0300

    Fix: added no favorites image

--- src/screens/Favorites/index.tsx
+++ src/screens/Favorites/index.tsx
@@ -1,12 +1,67 @@
-import React from 'react';
-import {Text} from 'react-native';
+import PokemonCard from '@/components/PokemonCard';
+import {useFavorites} from '@/hooks/useFavorites';
+import {useFocusEffect} from '@react-navigation/native';
+import React, {useCallback} from 'react';
+import {FlatList, StatusBar} from 'react-native';
+import {useTheme} from 'styled-components/native';
+import {
+  Container,
+  ContainerImage,
+  NotFavoritesImage,
+  styles,
+  Title,
+  SubTitle,
+} from './styles';
 
-import {Container} from './styles';
+const NoPokemonsAdded: React.FC = () => {
+  return (
+    <ContainerImage>
+      <Title>You haven't added any{'\n'}Pokemons yet.</Title>
+      <SubTitle>Go catch some Pokemons on{'\n'}the Home screen</SubTitle>
+      <NotFavoritesImage
+        source={require('@/assets/images/pikachuPhone.png')}
+        style={styles.image}
+      />
+    </ContainerImage>
+  );
+};
 
-const Favorites = () => {
+const Favorites: React.FC = () => {
+  const {loadFavorites, favoritesPokemon} = useFavorites();
+  const theme = useTheme();
+
+  const renderItem = useCallback(({item, index}) => {
+    return <PokemonCard index={index} data={item} />;
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      loadFavorites();
+    }, []),
+  );
+
   return (
     <Container>
-      <Text>Favorites</Text>
+      <StatusBar
+        animated={true}
+        translucent={true}
+        barStyle="dark-content"
+        backgroundColor={theme.COLORS.BACKGROUND}
+      />
+      {favoritesPokemon && favoritesPokemon.length > 0 ? (
+        <FlatList
+          data={favoritesPokemon}
+          keyExtractor={item => String(item.id)}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          renderItem={renderItem}
+          contentContainerStyle={{
+            paddingHorizontal: 24,
+          }}
+        />
+      ) : (
+        <NoPokemonsAdded />
+      )}
     </Container>
   );
 };
--- src/screens/Favorites/styles.ts
+++ src/screens/Favorites/styles.ts
@@ -1,7 +1,46 @@
+import {Image, StyleSheet} from 'react-native';
+import {getStatusBarHeight} from 'react-native-iphone-x-helper';
+import {RFValue} from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
+
+export const styles = StyleSheet.create({
+  image: {
+    resizeMode: 'contain',
+  },
+});
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${({theme}) => theme.COLORS.BACKGROUND};
+  padding: 12px 0px;
+  margin-top: ${getStatusBarHeight()}px;
+`;
+
+export const ContainerImage = styled.View`
+  flex: 1;
   align-items: center;
   justify-content: center;
+  margin-top: 48px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({theme}) => theme.FONTS.BOLD};
+  color: ${({theme}) => theme.COLORS.TITLE};
+
+  text-align: center;
+  font-size: 22px;
+  margin-bottom: 8px;
+`;
+
+export const SubTitle = styled.Text`
+  font-family: ${({theme}) => theme.FONTS.REGULAR};
+  color: ${({theme}) => theme.COLORS.TEXT_DARK};
+  text-align: center;
+  font-size: 16px;
+`;
+
+export const NotFavoritesImage = styled(Image)`
+  width: ${RFValue(150)}px;
+  height: ${RFValue(150)}px;
+  margin-top: 32px;
 `;
