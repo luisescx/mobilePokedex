18779153a37e15bc69289ff8e69372d45f6ac775
luisescx <luis.esculapio@live.com>
2022-02-28 22:17:58 -0300

    Feat: added splash screen on app.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import React, {useEffect} from 'react';
 import {ThemeProvider} from 'styled-components/native';
 import theme from '@/theme/index';
 import {Routes} from '@/routes';
 import {FavoritesProvider} from '@/contexts/FavoritesProvider';
+import {LogBox} from 'react-native';
+
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <FavoritesProvider>
       <ThemeProvider theme={theme}>
